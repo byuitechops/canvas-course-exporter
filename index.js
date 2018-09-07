0ab24d3ee7759374fde929934c39df963af4f5b3
Seth Childers <sethchilders92@gmail.com>
2018-09-07 15:15:30 -0600

    Made the functions dealing with path easier to read. Also added documentation, and made teh path and the directory name user specified now so that the script can be run globally

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const canvas = require('canvas-api-wrapper');
 const fs = require('fs');
+const path = require('path');
 const d3 = require('d3-dsv');
-const del = require('del');
 const moment = require("moment")
+const del = require('del');
 const queue = require('promise-limit')(5);
 const got = require('got');
 const timer = require('repeat-timer');
 const ProgressBar = require('progress');
-const path = require('path');
 
 // The user input from the cli
 let input;
@@ -40,14 +40,15 @@ function getCourses(userInput) {
  * @param {Object} course The course's name and id
  * @param {Object} bar The progress bar that is updated once the course is exported and downloaded
  * @param {String} folderDate The date string used to name the version folder and each course backup file
+ * @param {String} newDirectoryPath The path to the directory where things will be saved to 
  */
-async function exportCourse(course, bar, folderDate) {
+async function exportCourse(course, bar, folderDate, newDirectoryPath) {
     try {
         // start the course export in a ".imscc" format
         let contentExport = await canvas.post(`/api/v1/courses/${course.courseId}/content_exports`, {
             export_type: 'common_cartridge',
             skip_notifications: true
-        });    
+        });
 
         // log when it starts a course
         bar.interrupt(`Downloading ${course.courseName}`);
@@ -57,48 +58,49 @@ async function exportCourse(course, bar, folderDate) {
         do {
             await new Promise(res => setTimeout(res, 500));
             progress = await canvas(contentExport.progress_url);
-        } while (progress.workflow_state !== 'completed' && progress.completion !== 100);    
+        } while (progress.workflow_state !== 'completed' && progress.completion !== 100);
 
         // download the exported course with the content export
         let downloadedCourse = await canvas(`/api/v1/courses/${course.courseId}/content_exports/${contentExport.id}`);
         let url = downloadedCourse.attachment.url;
 
-
-        
-        // make a 'courseBackups' directory if it doesn't already exist
-        if (!fs.existsSync(path.resolve('courseBackups'))) {
-            fs.mkdirSync(path.resolve('courseBackups'));
-        }    
+        // make a new directory to put the version directories in if one doesn't already exist with the given name
+        if (!fs.existsSync(newDirectoryPath)) {
+            fs.mkdirSync(newDirectoryPath);
+        }
 
         // make a 'current date' directory if it doesn't already exist for this set of backup courses
-        if (!fs.existsSync(path.resolve(`courseBackups/${folderDate}`))) {
-            fs.mkdirSync(path.resolve(`courseBackups/${folderDate}`));
-        }    
+        if (!fs.existsSync(path.join(newDirectoryPath, folderDate))) {
+            fs.mkdirSync(path.join(newDirectoryPath, folderDate));
+        }
 
         // now that you have the url, pipe the contents of the url to a file
         await new Promise((res, rej) => {
-            got.stream(url).pipe(fs.createWriteStream(path.resolve(`courseBackups/${folderDate}/${course.courseName}-ID${course.courseId}-${folderDate}.imscc`)))
+            got.stream(url).pipe(fs.createWriteStream(path.join(newDirectoryPath, folderDate, `${course.courseName}-ID${course.courseId}-${folderDate}.imscc`)))
                 .on('finish', res)
                 .on('error', rej)
-        });        
+        });
 
         // increment the progress bar
         bar.tick(1);
     } catch (err) {
         console.error(err);
-    }    
-}    
+    }
+}
 
 /**
  * After backing up all the courses, check if there are too many versions saved,
  * based on the input from the user, and delete all of the older versions
+ * @param {String} newDirectoryPath The path to the directory where things will be saved to
  */
-async function checkVersions() { // I don't think this needs to be asynchronous
-    let fileNames = fs.readdirSync('courseBackups');
+async function checkVersions(newDirectoryPath) {
+    // fs.readdirSync returns a list of all files and directories in a directory,
+    // but there should only be directories inside of this new directory
+    let folderNames = fs.readdirSync(newDirectoryPath);
     // check if any existing versions need to be deleted
-    if (fileNames.length > input.versions) {
-        let versions = fileNames.map(file => {
-            let stats = fs.statSync(`courseBackups/${file}`);
+    if (folderNames.length > input.versions) {
+        let versions = folderNames.map(file => {
+            let stats = fs.statSync(path.join(newDirectoryPath, file));
             return {
                 birthtime: stats.birthtime,
                 name: file
@@ -113,11 +115,11 @@ async function checkVersions() { // I don't think this needs to be asynchronous
         });
 
         // delete the oldest version
-        let deletedVersion = del.sync([`courseBackups/${versions[0].name}`]);
+        let deletedVersion = del.sync([path.join(newDirectoryPath, versions[0].name)]);
         console.log(`Deleted version directory ${deletedVersion[0]}`);
 
         // run the function recursively as long as there are more version directories than the user specified amount
-        checkVersions();
+        checkVersions(newDirectoryPath);
     }
     return;
 }
@@ -127,6 +129,9 @@ async function checkVersions() { // I don't think this needs to be asynchronous
  */
 async function main() {
     try {
+        // the path to the new directory
+        let newDirectoryPath = path.resolve(path.join(input.saveDirectoryPath, input.saveDirectory));
+
         // get an array of courses that need to be backed-up
         let courses = getCourses(input);
         let bar = new ProgressBar(' Backing up courses [:bar] :percent', {
@@ -135,17 +140,17 @@ async function main() {
             incomplete: ' ',
             width: 20
         });
-        
+
         // format the current date
         let folderDate = moment().format("YYYY[Y]-MM[M]-DD[D] kk[h]mm[m]");
         console.log(`Starting course backups on ${folderDate}`);
 
         // setup the queue to run exportCourse in parallel (i.e. run it for a specified number of courses at a time)
-        let promises = courses.map(course => queue(() => exportCourse(course, bar, folderDate)));
+        let promises = courses.map(course => queue(() => exportCourse(course, bar, folderDate, newDirectoryPath)));
         await Promise.all(promises);
 
         // delete the oldest versions if there are more than the specified number of versions to be kept
-        await checkVersions();
+        await checkVersions(newDirectoryPath);
         console.log('All courses have been backed up successfully', '\n');
     } catch (err) {
         console.error(err);
